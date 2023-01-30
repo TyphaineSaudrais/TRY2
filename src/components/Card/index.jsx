f19eaa75aedca6d2d93f03aad8841ebf8d959d38
Typhaine Prevost <saudraisprevosttyphaine@gmail.com>
2023-01-30 17:25:04 +0100

    adding info on accomodation page

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,15 @@
-
+import { Link } from "react-router-dom";
 
 function AppartCard ({ id, title , cover }) {
 
+  
+
     return (
     <div key={id} className='appart-card'>
-            <a href="/Logements/:{id}">
+         <Link to={`/Logements/${id}`}>
             <img className='appart-img' src={cover} alt={{title}} />
             <p>{title}</p>
-            </a>
+        </Link>
     </div>
 )
 
--- src/pages/Logements/index.jsx
+++ src/pages/Logements/index.jsx
@@ -2,7 +2,7 @@ import { AppartList } from "../../Datas/AppartList"
 /* import Presentation from "../../components/Presentation" */
 import '../../utils/style/Logements.css';
 import Background from '../../assets/Background.png';
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
 function Logements() {
 
@@ -13,6 +13,10 @@ function Logements() {
     const LogementDisplayed = AppartList.find((Appart) => Appart.id === id);
 
 	if (LogementDisplayed) {
+
+        const equipments = LogementDisplayed.equipments.map((equipments) => <li>{equipments}</li>)
+        const tags = LogementDisplayed.tags.map((tags) => <li>{tags}</li>)
+
         return (
             <div>
                 <img src={Background} alt="banner Logements" className='Logements-banner'/>
@@ -20,12 +24,45 @@ function Logements() {
                     <div className='Logement-presentation'>
                         <h2>{LogementDisplayed.title}</h2>
                         <p>{LogementDisplayed.location}</p>
+                        <p>{LogementDisplayed.rating}</p>
+                        
                     </div>
+                    <div className='Logement-tags'>
+                        <ul>
+                            {tags}
+                        </ul>
+                   </div>
+                   <div className='Logement-host'>
+                        <h4>
+                            {LogementDisplayed.host.name}
+                        </h4>
+                        <img src={LogementDisplayed.host.picture} alt="host presentation"/>
+
+                   </div>
+                    <div className='Logement-description'>
+                        <h3>
+                            Description 
+                        </h3>
+                        <ul>
+                            {LogementDisplayed.description}
+                        </ul>
+                   </div>
+                   <div className='Logement-equipements'>
+                        <h3>
+                            Equipements
+                        </h3>
+                        <ul>
+                            {equipments}
+                        </ul>
+                   </div>
+                   
                 </div>
             </div>
         )
     }
-	
+	else {
+        <Navigate to= "*"/>
+    }
 }
 
 export default Logements
